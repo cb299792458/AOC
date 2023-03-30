eb055ce36875628588ad6cf49e091ecb193522e0
Brian <brianrlam@gmail.com>
2023-03-29 20:30:05 -0400

    do day 1 in js

--- day01/count.js
+++ day01/count.js
@@ -3,6 +3,25 @@ console.log('Counting the Calories');
 const fs = require('fs');
 
 const allFileContents = fs.readFileSync('input.txt', 'utf-8');
-allFileContents.split(/\r?\n/).forEach(line =>  {
-  console.log(`Line from file: ${line}`);
-});
+
+const arr = [];
+// allFileContents.split(/\r?\n/).forEach(line =>  {
+//   console.log(`Line from file: ${line}`);
+// });
+
+console.log(allFileContents.split(/\r?\n/))
+let temp = [];
+let max = 0;
+for(let str of allFileContents.split(/\r?\n/)){
+    if(str){
+        temp.push(parseInt(str));
+    } else {
+        let sum = temp.reduce((a,c)=>a+c)
+        max = Math.max(max,sum);
+
+
+        arr.push(temp);
+        temp = [];
+    }
+}
+console.log(max)
