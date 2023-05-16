d3c14dffbe3b44844a219f5bf1cde881dfd20822
Brian <brianrlam@gmail.com>
2023-05-16 11:12:29 -0400

    finish 18

--- day18/surface_area.py
+++ day18/surface_area.py
@@ -18,32 +18,37 @@ sa=0
 cubes=set(points)
 seen=set()
 
-
-# # dfs search
-# def explore(tup):
-#     global sa
-#     if tup in seen: return False
-
-#     seen.add(tup)
-
-#     for d in dirs:
-#         # check all 6 directions
-#         new=(tup[0]+d[0],tup[1]+d[1],tup[2]+d[2])
-
-#         if new not in cubes: sa+=1
-#         else: explore(new)
-
-# for pt in points:
-#     explore(pt)
-# print(sa)
-
-
+# part 1
 sa=6*len(cubes)
 for tup in points:
-    # global sa
     for d in dirs:
         # check all 6 directions
         new=(tup[0]+d[0],tup[1]+d[1],tup[2]+d[2])
 
         if new in cubes: sa-=1
 print(sa)
+
+
+# part 2
+osa=0
+queue = [(0,0,0)]
+while queue:
+    curr=queue.pop(0)
+
+    oob=False
+    for c in curr:
+        if c < -1 or c > 25: oob=True
+    if oob: continue
+
+    if curr in seen: continue
+    seen.add(curr)
+
+    for d in dirs:
+        new=(curr[0]+d[0],curr[1]+d[1],curr[2]+d[2])
+        if new in cubes:
+            osa+=1
+            continue
+        else: 
+            queue.append(new)
+
+print(osa)
