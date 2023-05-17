c25a8781823675e152dd2f02d1a1b6fad9977c38
Brian <brianrlam@gmail.com>
2023-05-16 20:44:05 -0400

    solve d16p1 w/ random results?!

--- day16/valves.py
+++ day16/valves.py
@@ -19,13 +19,13 @@ class Valve:
         for a in to_open:
             if a==self: continue
             self.dists.append((a.name,dijkstra_distance(self,a)))
-        self.dists.sort(key=lambda x: valves[x[0]].flow / x[1], reverse=True)
+        # self.dists.sort(key=lambda x: valves[x[0]].flow / x[1], reverse=True)
 
         self.dist_dict = dict()
         for a in self.dists:
             self.dist_dict[a[0]] = a[1]
 
-
+# parse input
 for line in input:
     words = line.split()
     name=words[1]
@@ -34,6 +34,7 @@ for line in input:
     neighbors = [n.strip(',') for n in neighbors]
     valves[name] = Valve(name,flow,neighbors)
 
+# make adjacency list
 to_open = set()
 for v in valves.values():
     v.make_adj_list()
@@ -50,22 +51,20 @@ def dijkstra_distance(start,end):
         queue=new_queue
         dist+=1
 
+# make dict of distance to each other relevant value
 for node in to_open:
     node.make_dist_list()
 valves['AA'].make_dist_list()
-# print(valves['AA'].dist_dict)
 
-class search:
+class Search:
     def __init__(self,node,opened,time_left,pressure) -> None:
         self.node = node
         self.opened = opened
         self.time_left = time_left
         self.pressure = pressure
 
-
-# res = 0
-# start = {'current':'AA', 'opened':set(), 'time_left':30, 'pressure':0}
-start = search(valves['AA'],set(),30,0)
+# bfs w/ each possible step as instance of "search" class
+start = Search(valves['AA'],set(),30,0)
 best = {30:0} # key is time_left, value is pressure
 queue = deque([start])
 
@@ -74,7 +73,6 @@ while queue:
 
     # end case
     if curr.time_left<0: 
-        # res = max(res, curr[''])
         continue
 
     # too slow
@@ -84,7 +82,7 @@ while queue:
     if too_slow: continue
 
     # save best pressure
-    if curr.time_left not in best or best[time_left] < curr.pressure:
+    if curr.time_left not in best or best[curr.time_left] < curr.pressure:
         # print('hit')
         best[curr.time_left] = curr.pressure
 
@@ -97,8 +95,14 @@ while queue:
         time_left = curr.time_left
         time_left -= curr.node.dist_dict[next_node.name] # travel time
         time_left -= 1 # time to open valve
-        queue.append(search(next_node, seen, time_left, curr.pressure+(time_left*next_node.flow)))
+        queue.append(Search(next_node, seen, time_left, curr.pressure+(time_left*next_node.flow)))
 
     # if curr.time_left==1: print(len(seen))
 
+# the value with the lowest key should be the best pressure, but it changes?!
 print(best)
+
+res = 0
+for val in best.values():
+    res = max(res,val)
+print(res)
