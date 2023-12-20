12f7ec2b4ce73bd1f2610e72e9a8803defa0f8a9
Brian <brianrlam@gmail.com>
2023-12-20 12:21:03 -0500

    finish 20 part 1

---
+++ 2023/20/pulses.py
@@ -0,0 +1,54 @@
+from collections import defaultdict, deque
+input = [l[:-1] for l in open('input.txt','r').readlines()]
+modules = dict()
+
+for line in input:
+    module, dests = line.split(' -> ')
+    dests = dests.split(', ')
+    mod_type, name = module[0], module[1:]
+
+    modules[name] = [mod_type, 0,     dict(),  dests]
+    #                % or &    % on   & memory outputs
+    for dest in dests:
+        if dest not in modules:
+            modules[dest] = ['output', 0, {}, []]
+
+for name, (_, _, _, dests) in modules.items():
+    for dest in dests:
+        if modules[dest][0] == '&':
+            modules[dest][2][name] = 0
+# print(modules)
+
+q = deque()
+pulses = [0,0]
+
+for _ in range(1000):
+    pulses[0] += 1
+    q.append(('roadcaster',  0,     'button'))
+    #         name, pulse, origin
+    while q:
+        # print(q)
+        (name, pulse, orig) = q.popleft()
+        module = modules[name]
+        [mod_type, on, memory, dests] = module
+
+        match mod_type:
+            case 'b':
+                for dest in dests:
+                    q.append((dest, pulse, name))
+                    pulses[pulse] += 1
+            case '%':
+                if not pulse:
+                    modules[name][1] = 0 if on else 1
+                    for dest in dests:
+                        q.append((dest, modules[name][1], name))
+                        pulses[modules[name][1]] += 1
+            case '&':
+                memory[orig] = pulse
+                send = 0 if all(memory.values()) else 1
+                for dest in dests:
+                    q.append((dest, send, name))
+                    pulses[send] += 1
+
+print(pulses)
+print(pulses[0]*pulses[1])
