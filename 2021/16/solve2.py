88237593f207e40fa150873703d239acc96d4301
brian <brianrlam@gmail.com>
2023-12-19 19:44:38 -0500

    sick of binary

--- 2021/16/solve2.py
+++ 2021/16/solve2.py
@@ -1,63 +1,67 @@
-import sys
-inf = sys.argv[1] if len(sys.argv) > 1 else 'input.txt'
+input = [l[:-1] for l in open('input.txt','r').readlines()]
+input = input[0]
 
-ll = [x for x in open(inf).read().strip().split('\n')]
-parse = {'0':'0000',
-'1':'0001',
-'2':'0010',
-'3':'0011',
-'4':'0100',
-'5':'0101',
-'6':'0110',
-'7':'0111',
-'8':'1000',
-'9':'1001',
-'A':'1010',
-'B':'1011',
-'C':'1100',
-'D':'1101',
-'E':'1110',
-'F':'1111'}
+binary = ''
+for c in input:
+    part = bin(int(c,16))[2:]
+    binary += '0'*(4-len(part)) + part
 
-data = ll[0]
-data = "".join([parse[x] for x in data])
-print(len(data))
+def parse(binary):
+    version = int(binary[:3],2)
+    type = int(binary[3:6],2)
+    binary = binary[6:]
 
-sumofversions=0
-def parse(data):
-    global sumofversions
-    version = int(data[:3],2)
-    sumofversions+=version
-    data=data[3:]
-
-    tid = int(data[:3],2)
-    data=data[3:]
-    if tid == 4:
-        t = ""
+    if type==4:
+        lit_val = ''
         while True:
-            t += data[1:5]
-            cnt = data[0]
-            data=data[5:]
-            if cnt == '0':
+            first = binary[0]
+            lit_val += binary[1:5]
+            binary=binary[5:]
+            if first=='0':
                 break
+        return [int(lit_val,2),binary]
     else:
-        ltid= data[0]
-        data=data[1:]
-        if ltid == '0':
-            l = data[:15]
-            data=data[15:]
-            subpacketslen = int(l, 2)
-            subpackets = data[:subpacketslen]
-            while subpackets:
-                subpackets = parse(subpackets)
-            data=data[subpacketslen:]
-        else:
-            l = data[:11]
-            data=data[11:]
-            subpacketsqty = int(l, 2)
-            for i in range(subpacketsqty):
-                data=parse(data)
-    return data
+        length_type_id = binary[0]
+        binary = binary[1:]
+        subvalues = []
 
-parse(data)
-print(sumofversions)
+        if length_type_id=='0':
+            length = int(binary[:15],2)
+            binary = binary[15:]
+            subpackets = binary[:length]
+
+            while subpackets:
+                [value, new_subpackets] = parse(subpackets)
+                subvalues.append(value)
+                subpackets = new_subpackets
+            binary = binary[length:]
+        else:
+            num_subpackets = int(binary[:11],2)
+            binary = binary[11:]
+            for _ in range(num_subpackets):
+                [value, new_binary] = parse(binary)   
+                subvalues.append(value)
+                binary = new_binary
+
+        match type:
+            case 0:
+                return [sum(subvalues), binary]
+            case 1:
+                product = 1
+                for val in subvalues:
+                    product *= val
+                return [product, binary]
+            case 2:
+                return [min(subvalues), binary]
+            case 3:
+                return [max(subvalues), binary]
+            case 5:
+                return [1 if subvalues[0]>subvalues[1] else 0, binary]
+            case 6:
+                return [1 if subvalues[0]<subvalues[1] else 0, binary]
+            case 7:
+                return [1 if subvalues[0]==subvalues[1] else 0, binary]
+            case _:
+                print('uhoh')
+
+print(parse(binary))
