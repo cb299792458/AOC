204e9cb62dae050018d5dac21c74705aec896cd4
Brian Lam <brianrlam@gmail.com>
2024-10-31 20:10:33 -0400

    finish dumb bug in 23

--- 2021/23/solve.py
+++ 2021/23/solve.py
@@ -2,11 +2,10 @@ from collections import deque
 
 input = [l[:-1] for l in open('input.txt','r').readlines()][1:-1]
 input = [list(line) for line in input]
-# for line in input:
-#     print(''.join(line))
 
 A, B, C, D, E = "A", "B", "C", "D", "."
 costs = {A: 1, B: 10, C: 100, D: 1000}
+goals = {A: 3, B: 5, C: 7, D: 9}
 
 halls = []
 rooms = []
@@ -26,10 +25,10 @@ def make_key(grid):
         for char in line:
             res.append(char)
         while len(res)%13!=0:
-            res.append(E)
+            res.append(' ')
     return ''.join(res)
 def make_grid(key):
-    return [list(key[i:i+13]) for i in range(0, 39, 13)]
+    return [list(key[i:i+13]) for i in range(0, len(key), 13)]
 
 def has_path(t1,t2,grid):
     r1,c1 = t1
@@ -58,16 +57,12 @@ def has_path(t1,t2,grid):
     return True
 
 seen = dict()
-finished = "#...........####A#B#C#D###  #A#B#C#D#  #A#B#C#D#  #A#B#C#D#"
 finished = "#...........####A#B#C#D###  #A#B#C#D#  "
 
-# key = make_key(input)
-# print(key)
-# grid = make_grid(key)
-# for line in grid:
-#     print(line)
+# part 2
+if len(input) > 3:
+    finished = "#...........####A#B#C#D###  #A#B#C#D#    #A#B#C#D#    #A#B#C#D#  "
 
-# q = deque([])
 q = deque([(0, make_key(input))])
 while q:
     (cost, key) = q.popleft()
@@ -77,25 +72,39 @@ while q:
         else:
             continue
     seen[key] = cost
-    
+
+    if cost>100000:
+        continue
     if key == finished:
         continue
     if finished in seen and cost >= seen[finished]:
         continue
 
     grid = make_grid(key)
+
     for r1,c1 in rooms:
         if grid[r1][c1] == E:
-            continue
+            continue # no amphipod there to move
         char = grid[r1][c1]
         for r2,c2 in halls:
             if grid[r2][c2] != E:
-                continue
+                continue # target spot is not empty
             if not has_path((r1,c1),(r2,c2),grid):
-                continue
+                continue # cannot reach target spot
+
+            must_move = False
+            if c1 == goals[char]:
+                for r in range(r1+1, len(grid)):
+                    if grid[r][c1] != char:
+                        must_move = True
+            else:
+                must_move = True
+            if not must_move:
+                continue # amphipod doesn't need to move
 
             dist = manhattan((r1,c1),(r2,c2))
 
+            # move amphipod, enqueue next state, move it back
             grid[r1][c1] = E
             grid[r2][c2] = char
             q.append((cost + (costs[char] * dist), make_key(grid)))
@@ -104,40 +113,35 @@ while q:
     
     for r1,c1 in halls:
         if grid[r1][c1] == E:
-            continue
+            continue # no amphipod there to move
         char = grid[r1][c1]
         for r2,c2 in rooms:
             if grid[r2][c2] != E:
-                continue
+                continue # target spot is not empty
             if not has_path((r1,c1),(r2,c2),grid):
-                continue
+                continue # cannot reach target spot
 
-            if char==A and c2!=3:
-                continue
-            if char==B and c2!=5:
-                continue
-            if char==C and c2!=7:
-                continue
-            if char==D and c2!=9:
-                continue
+            if goals[char] != c2:
+                continue # not the right room for this amphipod
 
-            # if r2>0 and grid[r2-1][c2] in [A,B,C,D] and grid[r2-1][c2]!=char:
-            #     continue
-            # if r2<2 and grid[r2+1][c2] in [A,B,C,D] and grid[r2+1][c2]!=char:
-            #     continue
             occupied = False
-            for r3 in range(1, 3):
+            for r3 in range(r1+1, len(grid)):
                 if grid[r3][c2] in [A,B,C,D] and grid[r3][c2]!=char:
                     occupied = True
                     break
             if occupied:
-                continue
+                continue # different amphipod in that room
 
             dist = manhattan((r1,c1),(r2,c2))
 
+            # move amphipod, enqueue next state, move it back
             grid[r1][c1] = E
             grid[r2][c2] = char
             q.append((cost + (costs[char] * dist), make_key(grid)))
             grid[r1][c1] = char
             grid[r2][c2] = E
-print(seen[finished])
+
+if finished in seen:
+    print("solution:", seen[finished])
+else:
+    print("no solution found")
