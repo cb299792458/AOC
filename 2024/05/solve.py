54a69109354479d02d8f7c17ff9bd181549f4319
Brian <brianrlam@gmail.com>
2024-12-05 16:40:15 -0500

    finish 5

--- 2024/05/solve.py
+++ 2024/05/solve.py
@@ -1,1 +1,43 @@
+from collections import defaultdict
 input = [l[:-1] for l in open('input.txt','r').readlines()]
+
+prereqs = defaultdict(list)
+i = 0
+while input[i] != '':
+    a,b = input[i].split('|')
+    prereqs[b].append(a)
+    i += 1
+
+i += 1
+total = 0
+total2 = 0
+
+while i < len(input):
+    valid = True
+    pages = input[i].split(',')
+
+    cant = set()
+    for page in pages:
+        if page in cant:
+            valid = False
+            break
+        for p in prereqs[page]:
+            cant.add(p)
+    
+    if valid: # part 1
+        total += int(pages[len(pages)//2])
+    else: # part 2
+        ordered = []
+        while len(ordered) < len(pages):
+            for page in pages:
+                if page in ordered:
+                    continue
+                if all((p in ordered or p not in pages) for p in prereqs[page]):
+                    ordered.append(page)
+                    break
+        total2 += int(ordered[len(ordered)//2])
+
+    i+= 1
+print(total)
+print(total2)
+    
