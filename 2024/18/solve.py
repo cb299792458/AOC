e0de6e962b8279ef67d03b00be01875d8fb548d4
Brian Lam <brianrlam@gmail.com>
2024-12-18 10:43:05 -0500

    brute force 18

--- 2024/18/solve.py
+++ 2024/18/solve.py
@@ -1,1 +1,52 @@
 input = [l[:-1] for l in open('input.txt','r').readlines()]
+GRID_SIZE = 71
+BYTES = 1024
+
+grid = [['.' for _ in range(GRID_SIZE)] for _ in range(GRID_SIZE)]
+# for i in range(BYTES):
+#     [c, r] = input[i].split(',')
+#     c = int(c)
+#     r = int(r)
+#     grid[r][c] = '#'
+
+# for line in grid:
+#     print(''.join(line))
+
+from collections import deque
+
+def find_min_steps():
+    queue = deque([(0,0,0)])
+    visited = set()
+
+    while queue:
+        (steps, r, c) = queue.popleft()
+        if r<0 or r>=GRID_SIZE or c<0 or c>=GRID_SIZE:
+            continue
+        if grid[r][c] == '#':
+            continue
+        if (r,c) in visited:
+            continue
+        visited.add((r,c))
+
+        if r == GRID_SIZE-1 and c == GRID_SIZE-1:
+            return steps
+
+        queue.append((steps+1, r+1, c))
+        queue.append((steps+1, r-1, c))
+        queue.append((steps+1, r, c+1))
+        queue.append((steps+1, r, c-1))
+
+    return -1
+
+# print(find_min_steps())
+
+for [i, line] in enumerate(input):
+    print(i)
+    
+    [c, r] = line.split(',')
+    c = int(c)
+    r = int(r)
+    grid[r][c] = '#'
+    if find_min_steps() == -1:
+        print(f'[{c},{r}]')
+        break
