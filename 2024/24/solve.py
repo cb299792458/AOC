8060d2bca036cb37f8df060eaedecf740c234956
Brian Lam <brianrlam@gmail.com>
2024-12-24 10:05:27 -0500

    finish 24/1

--- 2024/24/solve.py
+++ 2024/24/solve.py
@@ -1,1 +1,42 @@
-input = [l[:-1] for l in open('input.txt','r').readlines()]
+from collections import defaultdict
+input = open('input.txt', 'r').read().strip()
+
+[start, gates] = input.split('\n\n')
+start = start.split('\n')
+gates = gates.split('\n')
+
+wires = defaultdict(lambda: -1)
+for line in start:
+    [key, val] = line.split(': ')
+    wires[key] = int(val)
+
+updated = True
+while updated:
+    updated = False
+    for line in gates:
+        [w1, op, w2, _, w3] = line.split(' ')
+
+        if wires[w1] == -1 or wires[w2] == -1:
+            continue
+        if wires[w3] != -1:
+            continue
+        match op:
+            case 'AND':
+                wires[w3] = wires[w1] & wires[w2]
+            case 'OR':
+                wires[w3] = wires[w1] | wires[w2]
+            case 'XOR':
+                wires[w3] = wires[w1] ^ wires[w2]
+        updated = True
+
+def read_num(char):
+    res = 0
+    i = 0
+    while True:
+        key = char + ('0' if i < 10 else '') + str(i)
+        if wires[key] == -1:
+            break
+        res += wires[key] * (2 ** i)
+        i+=1
+    return res
+print(read_num('z'))
