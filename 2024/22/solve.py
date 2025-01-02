8932ab64399aa0674da58a771a59c26823cf72d4
Brian <brianrlam@gmail.com>
2025-01-02 17:19:46 -0500

    finish 22

--- 2024/22/solve.py
+++ 2024/22/solve.py
@@ -1,5 +1,6 @@
-input = [l[:-1] for l in open('input.txt','r').readlines()]
+from collections import deque, defaultdict
 
+input = [l[:-1] for l in open('input.txt','r').readlines()]
 nums = [int(line) for line in input]
 
 def mix(value, secret):
@@ -13,9 +14,39 @@ def process(secret):
     secret = prune(mix(secret * 2048, secret))
     return secret
 
-total = 0
+# # part 1
+# total = 0
+# for num in nums:
+#     for _ in range(2000):
+#         num = process(num)
+#     total += num
+# print(total)
+
+# part 2
+totals = defaultdict(int)
 for num in nums:
+    prev = num
+    last_four = deque()
+    seen = set()
+
     for _ in range(2000):
         num = process(num)
-    total += num
-print(total)
+        last_price = prev % 10
+        price = num % 10
+        last_four.append(price - last_price)
+        if len(last_four) > 4:
+            last_four.popleft()
+        
+        if len(last_four) == 4:
+            key = tuple(last_four)
+            # if key == (-2,1,-1,3):
+            #     print(price)
+            if key not in seen:
+                seen.add(key)
+                totals[key] += price
+        
+        prev = num
+
+everything = list(totals.items())
+everything.sort(key=lambda x: x[1])
+print(everything[-1])
