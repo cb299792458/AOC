88b54084a2b71817822261bae8e8f886d51e4939
Brian <brianrlam@gmail.com>
2025-11-30 08:04:27 -0500

    finish 10

--- 2020/10/solve.py
+++ 2020/10/solve.py
@@ -1,1 +1,20 @@
+from collections import Counter
 input = [l[:-1] for l in open('input.txt','r').readlines()]
+input = list(map(int, input))
+
+input.sort()
+input.append(input[-1] + 3)
+
+prev = 0
+diffs = Counter()
+for num in input:
+    diffs[num - prev] += 1
+    prev = num
+
+print(diffs[1] * diffs[3])
+
+ways = Counter()
+ways[0] = 1
+for num in input:
+    ways[num] = ways[num-1] + ways[num-2] + ways[num-3]
+print(ways[input[-1]])
