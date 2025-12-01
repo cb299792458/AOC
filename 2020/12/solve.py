688480b995202dc807f28ac2d0a9af5f7f3b3de1
Brian Lam <brianrlam@gmail.com>
2025-12-01 15:40:15 -0500

    finish 12

--- 2020/12/solve.py
+++ 2020/12/solve.py
@@ -12,22 +12,44 @@ di = 0
 
 r, c = 0, 0
 
+# # part 1
+# for line in input:
+#     char = line[0]
+#     num = int(line[1:])
+
+#     if char in [N, S, W, E]:
+#         r += compass[char][0] * num
+#         c += compass[char][1] * num
+#     elif char == L:
+#         di -= num // 90
+#     elif char == R:
+#         di += num // 90
+#     elif char == F:
+#         r += directions[di%4][0] * num
+#         c += directions[di%4][1] * num
+#     else:
+#         raise Exception('unknown instruction')
+
+# print(abs(r) + abs(c))
+
+wr, wc = -1, 10
 for line in input:
     char = line[0]
     num = int(line[1:])
 
     if char in [N, S, W, E]:
-        r += compass[char][0] * num
-        c += compass[char][1] * num
-    elif char == L:
-        di -= num // 90
+        wr += compass[char][0] * num
+        wc += compass[char][1] * num
     elif char == R:
-        di += num // 90
+        for _ in range(num // 90):
+            [wr, wc] = [wc, -wr]
+    elif char == L:
+        for _ in range(num // 90):
+            [wr, wc] = [-wc, wr]
     elif char == F:
-        r += directions[di%4][0] * num
-        c += directions[di%4][1] * num
+        r += wr * num
+        c += wc * num
     else:
         raise Exception('unknown instruction')
 
 print(abs(r) + abs(c))
-
--- 2025/02/solve.py
+++ 2025/02/solve.py
@@ -1,1 +1,44 @@
 input = [l[:-1] for l in open('input.txt','r').readlines()]
+input = input[0]
+
+def is_invalid(num):
+    s = str(num)
+    N = len(s)
+    if N % 2:
+        return False
+    return s[:N//2] == s[N//2:]
+
+def factors(num):
+    res = []
+    for n in range(2, num + 1):
+        if not num % n:
+            res.append(n)
+    return res
+
+def parts(string, parts):
+    size = len(string) // parts
+    res = []
+    for i in range(0, len(string), size):
+        res.append(string[i:i+size])
+    return res
+
+def is_invalid2(num):
+    s = str(num)
+    for f in factors(len(s)):
+        p = parts(s, f)
+        if all([part == p[0] for part in p]):
+            return True
+    return False
+
+
+total = 0
+
+for num_range in input.split(','):
+    [start, final] = num_range.split('-')
+    for num in range(int(start), int(final) + 1):
+        if is_invalid2(num):
+            # print(num)
+            total += num
+
+print(total)
+
