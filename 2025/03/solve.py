fdb655b7dc530905cabf603c8f1d96f3195485a8
Brian Lam <brianrlam@gmail.com>
2025-12-03 09:36:44 -0500

    finish 2025-03-1

--- 2025/03/solve.py
+++ 2025/03/solve.py
@@ -1,1 +1,10 @@
 input = [l[:-1] for l in open('input.txt','r').readlines()]
+
+joltage = 0
+for line in input:
+    nums = [int(c) for c in line]
+    first = max(nums[:-1])
+    last = max(nums[nums.index(first)+1:])
+    joltage += first * 10 + last
+
+print(joltage)
