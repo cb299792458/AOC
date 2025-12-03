588503dca3533c644c00dcb6c55026d72971fe91
Brian Lam <brianrlam@gmail.com>
2025-12-03 09:01:58 -0500

    finish 2020-13-1

--- 2020/13/solve.py
+++ 2020/13/solve.py
@@ -1,1 +1,20 @@
+import math
+
 input = [l[:-1] for l in open('input.txt','r').readlines()]
+start = int(input[0])
+ids = input[1].split(',')
+
+least = math.inf
+res = 0
+for id in ids:
+    if id =='x':
+        continue
+    id = int(id)
+    prev = start % id
+
+    wait = id - prev
+    if wait < least:
+        least = wait
+        res = id * wait
+
+print(res)
