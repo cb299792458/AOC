b1afe1c0b698601d5a19790355f8d6b7dddbe8f5
Brian Lam <brianrlam@gmail.com>
2025-12-04 09:54:51 -0500

    finish 3

--- 2025/03/solve.py
+++ 2025/03/solve.py
@@ -1,10 +1,30 @@
+from functools import cache
 input = [l[:-1] for l in open('input.txt','r').readlines()]
+input = [[int(c) for c in line] for line in input]
+
+N = len(input[0])
+# joltage = 0
+# for line in input:
+#     nums = [int(c) for c in line]
+#     first = max(nums[:-1])
+#     last = max(nums[nums.index(first)+1:])
+#     joltage += first * 10 + last
+
+# print(joltage)
 
 joltage = 0
 for line in input:
-    nums = [int(c) for c in line]
-    first = max(nums[:-1])
-    last = max(nums[nums.index(first)+1:])
-    joltage += first * 10 + last
-
+    @cache
+    def dp(index, digits): # index goes backwards from N - 1 to 0
+        if index == -1: # llegue al comienzo de la linea
+            return 0
+        if digits == 12: # ya tiene 12 digitos
+            return 0
+        
+        skip = dp(index - 1, digits)
+        take = 10 * dp(index - 1, digits + 1) + line[index]
+        
+        return max(skip, take)
+    
+    joltage += dp(N - 1, 0)
 print(joltage)
