ad6e81c9e598fca0a01d659d8963a5d4715b4a68
Brian Lam <brianrlam@gmail.com>
2026-09-12 16:05:55 -0400

    update 2

--- 2024/02/solve.py
+++ 2024/02/solve.py
@@ -1,38 +1,49 @@
 input = [l[:-1] for l in open('input.txt','r').readlines()]
+lines = [[int(n) for n in line.split()] for line in input]
 
-safes = 0
+def check(line):
+    diffs = []
+    for [num1, num2] in zip(line, line[1:]):
+        diffs.append(num1 - num2)
+    if any(abs(diff) > 3 for diff in diffs):
+        return False
+    return all(diff > 0 for diff in diffs) or all(diff < 0 for diff in diffs)
 
-def is_safe(levels):
-    inc = False
-    dec = False
+print(sum(1 for line in lines if check(line)))
 
-    for i in range(1, len(levels)):
-        [curr, prev] = [levels[i], levels[i-1]]
-        if curr > prev:
-            inc = True
-        elif curr < prev:
-            dec = True
-        if abs(curr - prev) < 1 or abs(curr - prev) > 3:
-            return False
-    return inc ^ dec
+# safes = 0
 
-for report in input:
-    levels = list(map(int, report.split(' ')))
-    if is_safe(levels):
-        safes += 1
+# def is_safe(levels):
+#     inc = False
+#     dec = False
 
-print(safes)
+#     for i in range(1, len(levels)):
+#         [curr, prev] = [levels[i], levels[i-1]]
+#         if curr > prev:
+#             inc = True
+#         elif curr < prev:
+#             dec = True
+#         if abs(curr - prev) < 1 or abs(curr - prev) > 3:
+#             return False
+#     return inc ^ dec
 
-safes = 0
+# for report in input:
+#     levels = list(map(int, report.split(' ')))
+#     if is_safe(levels):
+#         safes += 1
 
-for report in input:
-    levels = list(map(int, report.split(' ')))
-    any_safe = False
-    for i in range(len(levels)):
-        skipped = levels[:i] + levels[i+1:]
-        if is_safe(skipped):
-            any_safe = True
+# print(safes)
+
+# safes = 0
+
+# for report in input:
+#     levels = list(map(int, report.split(' ')))
+#     any_safe = False
+#     for i in range(len(levels)):
+#         skipped = levels[:i] + levels[i+1:]
+#         if is_safe(skipped):
+#             any_safe = True
     
-    if any_safe:
-        safes += 1
-print(safes)
+#     if any_safe:
+#         safes += 1
+# print(safes)
