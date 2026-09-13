d9351b2476de5ec705d5440cf75b32db7c4fbbb6
Brian Lam <brianrlam@gmail.com>
2026-09-13 15:45:37 -0400

    finish 15 (brute force)

--- 2020/15/solve.py
+++ 2020/15/solve.py
@@ -1,1 +1,26 @@
+from collections import defaultdict
 input = [l[:-1] for l in open('input.txt','r').readlines()]
+input = input[1]
+input = [int(n) for n in input.split(',')]
+
+last_spoken = defaultdict(list)
+turn = 0
+
+nums = []
+
+while turn < 30000000:
+    if turn < len(input):
+        curr = input[turn]
+    else:
+        last_num = nums[-1]
+
+        if len(last_spoken[last_num]) < 2:
+            curr = 0
+        else:
+            curr = last_spoken[last_num][-1] - last_spoken[last_num][-2]
+
+    nums.append(curr)
+    last_spoken[curr].append(turn)
+    turn += 1
+
+print(nums[-1])
